2e420701326236c1efae745e7e35f561ce1463b1
dave <information@com1software.com>
2022-09-23 09:42:25 -0400

    update

--- clone.sh
+++ clone.sh
@@ -1,1 +1,31 @@
 #!/bin/sh
+cd ..
+git clone https://github.com/Com1Software/Test-Pi-GPS.git
+cd Test-Pi-GPS
+go mod init test
+go mod tidy
+go build
+
+cd ..
+git clone https://github.com/Com1Software/Test-Pi-Servo.git
+cd Test-Pi-Servo
+go mod init test
+go mod tidy
+go build
+
+cd ..
+git clone https://github.com/Com1Software/GPS-Track-Recorder.git
+cd GPS-Track-Recorder
+go mod init test
+go mod tidy
+go build
+
+cd ..
+git clone https://github.com/Com1Software/GPS-Track-Player.git
+cd GPS-Track-Player
+go mod init test
+go mod tidy
+go build
+
+cd ..
+return
