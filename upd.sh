f16550b4862d31393ac1ee8588974c4332acd617
dave <information@com1software.com>
2022-09-23 09:45:47 -0400

    update

--- upd.sh
+++ upd.sh
@@ -1,1 +1,27 @@
 #!/bin/sh
+cd ..
+cd Test-Pi-GPS
+git pull
+go mod tidy
+go build
+
+cd ..
+cd Test-Pi-Servo
+git pull
+go mod tidy
+go build
+
+cd ..
+cd GPS-Track-Recorder
+git pull
+go mod tidy
+go build
+
+cd ..
+cd GPS-Track-Player
+git pull
+go mod tidy
+go build
+
+cd ..
+return
