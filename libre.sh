c0e2213a4e285251f28b83e37fab1f9a80d71b33
Com1 Software, Inc <information@com1software.com>
2022-10-06 13:07:08 -0400

    Update libre.sh

--- libre.sh
+++ libre.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 git clone https://github.com/libre-computer-project/libretech-raspbian-portability.git lrp
 sudo lrp/oneshot.sh aml-s905x-cc
+git clone https://github.com/libre-computer-project/libretech-wiring-tool.git
+cd libretech-wiring-tool/
+make
+sudo ./ldto enable i2c-ao
+
 
